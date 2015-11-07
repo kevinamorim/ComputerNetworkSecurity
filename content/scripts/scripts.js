f7a8137f85a33e153a1c1de0d1d3c97cc00b3616
Kevin Amorim <kevinamorim4@gmail.com>
2015-11-07 18:37:05 +0100

    Vigenère Cipher implemented & Fixed bug

--- content/scripts/scripts.js
+++ content/scripts/scripts.js
@@ -1,21 +1,4 @@
-function say_hello() {
-	
-	var e = document.getElementById("shift");
-	var shiftNumber = parseInt(e.options[e.selectedIndex].value);
-	
-	var originalAlphabet = "ABCDEFGHIJLMNOPQRSTUVXZ";
-	var newAlphabet = "";
-	
-	for(var i = 0; i < 23; i++) {
-		var newNum = (i + shiftNumber) % 23;
-		var code = originalAlphabet.charCodeAt(newNum);
-		newAlphabet += String.fromCharCode(code);
-	}
-	
-	alert(newAlphabet);
-}
-
-function say_hello_2() {
+function cipher_caesar() {
 	
 	var e = document.getElementById("shift");
 	var shiftNumber = parseInt(e.options[e.selectedIndex].value);
@@ -35,7 +18,7 @@ function say_hello_2() {
 	
 }
 
-function decipher() {
+function decipher_caesar() {
 	
 	var e = document.getElementById("shift");
 	var shiftNumber = parseInt(e.options[e.selectedIndex].value);
@@ -46,11 +29,49 @@ function decipher() {
 	for(var i = 0; i < strToCipher.length; i++) {
 		var charCodeOriginal = strToCipher.charCodeAt(i);
 		charCodeOriginal = charCodeOriginal - 65;
-		var newCode = ((charCodeOriginal - shiftNumber) % 26) + 65;
+		var newCode = ((((charCodeOriginal - shiftNumber) % 26) + 26) % 26) + 65;
 		cipher += String.fromCharCode(newCode);
 	}
 	
 	var box = document.getElementById('cipher');
 	box.value = cipher;
 	
+}
+
+function cipher_vigenere() {
+	
+	var key = document.getElementById("key").value;
+	var strToCipher = document.getElementById("textToCipher").value;
+	var cipher = "";
+	
+	for(var i = 0; i < strToCipher.length; i++) {
+		var keyCode = key.charCodeAt(i % key.length);
+		var charCodeOriginal = strToCipher.charCodeAt(i);
+		charCodeOriginal = charCodeOriginal - 65;
+		var newCode = ((charCodeOriginal + keyCode) % 26) + 65;
+		cipher += String.fromCharCode(newCode);
+	}
+	
+	var box = document.getElementById('cipher');
+	box.value = cipher;
+		
+}
+
+function decipher_vigenere() {
+	
+	var key = document.getElementById("key").value;
+	var strToCipher = document.getElementById("textToCipher").value;
+	var cipher = "";
+	
+	for(var i = 0; i < strToCipher.length; i++) {
+		var keyCode = key.charCodeAt(i % key.length);
+		var charCodeOriginal = strToCipher.charCodeAt(i);
+		charCodeOriginal = charCodeOriginal - 65;
+		var newCode = ((((charCodeOriginal - keyCode) % 26) + 26) % 26) + 65;
+		cipher += String.fromCharCode(newCode);
+	}
+	
+	var box = document.getElementById('cipher');
+	box.value = cipher;
+		
 }

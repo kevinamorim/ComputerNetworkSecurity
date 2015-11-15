6b3cd9525f2fbcbafa1579d20b357d06814e4f52
Kevin Amorim <kevinamorim4@gmail.com>
2015-11-15 22:03:11 +0100

    Style updated

--- content/scripts/scripts.js
+++ content/scripts/scripts.js
@@ -77,4 +77,79 @@ function decipher_vigenere() {
 		
 }
 
+function work() {
+	
+	if (!$("input[name='method']:checked").val()) {
+		alert("CHOOSE A FUCKING METHOD");
+		return;
+	}
+	
+	if (!$("input[name='action']:checked").val()) {
+		alert("WHAT DO U WANT TO DO FUCKER??");
+		return;
+	}
+	
+	if (!$("input[name='message']").val()) {
+		alert("cmon man");
+		$("input[name='message']").focus();
+		return;
+	}
+	
+	var method = document.querySelector('input[name="method"]:checked').value;
+	
+	if(method == 'vigenere') {
+		if (!$("input[name='key']").val()) {
+			alert("fuck a key");
+			$("input[name='key']").focus();
+			return;
+		}
+	}
+	
+	var action = document.querySelector('input[name="action"]:checked').value;
+	
+	var text = document.getElementById("textToCipher").value;
+	var result = "";
+	
+	if(method == 'caesar') {
+		var e = document.getElementById("offset");
+		var shiftNumber = parseInt(e.options[e.selectedIndex].value);
+		var key = String.fromCharCode(shiftNumber + 65);
+		if(action == 'cipher') {
+			result = cipher(text.toUpperCase(), key.toUpperCase());
+		} else {
+			result = decipher(text.toUpperCase(), key.toUpperCase());
+		}
+	} else if(method == 'vigenere') {
+		var key = document.getElementsByName('key')[0].value;
+		if(action == 'cipher') {
+			result = cipher(text.toUpperCase(), key.toUpperCase());
+		} else {
+			result = decipher(text.toUpperCase(), key.toUpperCase());
+		}
+	}
+	
+	var box = document.getElementById('result');
+	box.value = result;
+	
+}
+
+function about() {
+	alert("Kevin Amorim");
+}
+
+$(document).ready(function () {
+	$('#textToCipher').keypress(function(e){
+	  if(e.keyCode==13)
+	  $('#work').click();
+	});
+	
+	$("input:text").focus(function() { $(this).select(); } );
+	$("#textToCipher").keyup(function() {
+		if($("input[name='message']").val())
+			work();
+		else
+			var box = document.getElementById('result');
+			box.value = "";
+	});
+});
 
